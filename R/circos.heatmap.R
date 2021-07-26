2de4c94f38fdff473eff6a679a438027083f3f85
tlz4320 <273109121@qq.com>
2021-07-26 23:01:29 +0800

    Update circos.heatmap.R

--- R/circos.heatmap.R
+++ R/circos.heatmap.R
@@ -330,7 +330,7 @@ circos.heatmap = function(mat, split = NULL, col, na.col = "grey",
 	dend.side = c("none", "outside", "inside"), dend.track.height = 0.1,
 	rownames.side = c("none", "outside", "inside"), rownames.cex = 0.5,
 	rownames.font = par("font"), rownames.col = "black", 
-	show.sector.labels = FALSE, cell_width = rep(1, nrow(mat)), cell_width2=0 ...) {
+	show.sector.labels = FALSE, cell_width = rep(1, nrow(mat)), cell_width2=0, ...) {
 
 	if(!is.matrix(mat)) {
 		if(is.vector(mat) && is.atomic(mat)) {
